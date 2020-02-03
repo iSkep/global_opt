dd19b0b177e495d6b399ceb6d82def19f25ea1d7
Skep <madskep@gmail.com>
2020-02-04 00:41:43 +0200

    Доделан скрипт слайдера (пока стоит временный костыль на масштабирование не-центральных слайдов)

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $('.feedback__slider').slick({
         centerMode: true,
-        centerPadding: '45px',
+        variableWidth: true,
+        centerPadding: '0px',
         slidesToShow: 3,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg" alt="left arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg" alt="right arrow"></button>',
