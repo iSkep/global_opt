7190af338bd1138dece8d24830358c92a562496a
Skep <madskep@gmail.com>
2020-02-18 00:12:16 +0200

    Добавлены модальные окна, скрипт под них. Прочие мелкие фиксы.

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,24 @@ $(document).ready(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg" alt="left arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg" alt="right arrow"></button>',
     });
-  });
+
+    // Modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('[data-modal=calculation]').on('click', function () {
+        $('.overlay, #calc').fadeIn();
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.modal, .overlay').fadeOut('fast');
+    });
+
+    $('.price-item .button').each(function (i) {
+        $(this).on('click', function () {
+            $('#details .modal__subtitle').text($('.price-item__header').eq(i).text()).css("text-transform", "uppercase");
+            $('.overlay, #details').fadeIn();
+        });
+    });
+});
